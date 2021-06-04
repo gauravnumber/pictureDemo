261cb082f35493dd9ae43db854eac66d72224014
Gaurav <gauravnumber@gmail.com>
2021-06-04 16:03:18 +0530

    added

---
+++ node_modules/gauravnumber-picture/index.js
@@ -0,0 +1,25 @@
+function picture(options) {
+	console.log('picture here through npm')
+  // let images = documents.querySelectorAll('.picture')
+
+  // if (options.width === 'big') {
+  // 	options.width = '100px'
+  // } else {
+  // 	options.width = '10px'
+  // }
+
+  // images.forEach(image => {
+  // 	image.style.width = options.width
+  // })
+}
+
+module.exports.picture = picture
+
+
+// export const picture = () => {
+// 	console.log('picture in const')
+// }
+
+// exports.picture = function () {
+// 	console.log('export.picture is here')
+// }
